77b05635e9faf12522138c4b7ac5ba17715bb04c
Samuel SAYAG <samouille666@hotmail.com>
2018-03-23 17:51:47 +0300

    implement traverse and sequence in term of traverse

--- src/main/scala/part/one/four/OptionExercize.scala
+++ src/main/scala/part/one/four/OptionExercize.scala
@@ -17,8 +17,19 @@ object OptionExercize {
   def map2[A, B, C](a: Option[A], b: Option[B])(f: (A, B) => C): Option[C] =
     a.flatMap(p1 => b.map(p2 => f(p1, p2)))
 
- 
+  // old version of sequence
+  /*  def sequence[A](a: List[Option[A]]): Option[List[A]] =
+      a.foldRight(Some(List.empty[A]): Option[List[A]])(
+        (oa, acc) => oa.flatMap(i => acc.map(i +: _)))*/
+
+
   def sequence[A](a: List[Option[A]]): Option[List[A]] =
-    a.foldRight(Some(List.empty[A]): Option[List[A]])(
-      (oa, acc) => oa.flatMap(i => acc.map(i +: _)))
+    traverse(a)(identity[Option[A]])
+
+
+  def traverse[A, B](a: List[A])(f: A => Option[B]): Option[List[B]] =
+    a.foldRight(Some(List.empty[B]): Option[List[B]])(
+      (a, acc) =>
+        f(a).flatMap(i => acc.map(i +: _)))
+
 }
