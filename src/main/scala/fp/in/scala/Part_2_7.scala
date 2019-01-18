d458ab0f6ee81e70e52a9bcc81a45a0608e8a91c
Samuel SAYAG <samouille666@hotmail.com>
2019-01-18 16:20:37 +0200

    advance in chp2

--- src/main/scala/fp/in/scala/Part_2_7.scala
+++ src/main/scala/fp/in/scala/Part_2_7.scala
@@ -10,7 +10,6 @@ object Part_2_7 {
   // sum of integer over a list
   def sumFold(s: Seq[Long]): Long = s.foldLeft(0L)(_ + _)
 
-
   def sumDC(s: IndexedSeq[Long]): Long = {
     if (s.length <= 1) s.headOption.getOrElse(0)
     else {
@@ -19,15 +18,33 @@ object Part_2_7 {
     }
   }
 
-  object FunParTest {
-    def testfold(s: Int) = sumFold(Seq.range(0, s - 1))
+  final case class Par[E](e: E)
 
-    def testDivConq(s: Int) = sumDC(IndexedSeq.range(0, s - 1))
+  object Par {
+    def unit[A](a: => A): Par[A] = ???
+
+    def get[A](pa: Par[A]): A = ???
+  }
+
+  def sumP(s: IndexedSeq[Long]): Long = {
+    if (s.length <= 1) s.headOption.getOrElse(0)
+    else {
+      val (l, r) = s.splitAt(s.length / 2)
+      val p1 = Par.unit(sumP(l))
+      val p2 = Par.unit(sumP(r))
+      Par.get(p1) + Par.get(p2)
+    }
+  }
+
+  object FunParTest {
+    def testfold(s: Int): Long = sumFold(Seq.range(0, s - 1))
+
+    def testDivConq(s: Int): Long = sumDC(IndexedSeq.range(0, s - 1))
   }
 
   def main(args: Array[String]): Unit = {
     import FunParTest._
-    println(testfold(20000000))  // execute in about 7 sec
-//    println(testDivConq(20000000)) // execute in about 6 sec why ???
+    //    println(testfold(20000000)) //[error] java.lang.OutOfMemoryError: GC overhead limit exceeded
+    println(testDivConq(20000000)) // this execute in about 60 sec
   }
 }
