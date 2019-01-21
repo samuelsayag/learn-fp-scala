48fbaa571d05ba5371dd0c212bbca2e551958e63
Samuel SAYAG <samuel.sayag@clicktale.com>
2019-01-21 10:50:49 +0200

    advance par

--- src/main/scala/fp/in/scala/Part_2_7.scala
+++ src/main/scala/fp/in/scala/Part_2_7.scala
@@ -1,5 +1,7 @@
 package fp.in.scala
 
+import java.util.concurrent.{Callable, ExecutorService, Future, TimeUnit}
+
 object Part_2_7 {
 
   // pure functional parallelism
@@ -18,19 +20,51 @@ object Part_2_7 {
     }
   }
 
-  final case class Par[E](e: E)
+  //  final case class Par[E](e: E)
+
+  type Par[A] = ExecutorService => Future[A]
 
   object Par {
 
-    def fork[A](a: => Par[A]):Par[A] = ???
+    def fork[A](a: => Par[A]): Par[A] = (es: ExecutorService) => {
+      es.submit(new Callable[A] {
+        override def call(): A = a(es).get()
+      })
+    }
 
-    def unit[A](a:  A): Par[A] = ???
+    def unit[A](a: A): Par[A] = (es: ExecutorService) => UnitFuture(a)
 
     def lazyUnit[A](a: => A): Par[A] = fork(unit(a))
 
-    def run[A](pa: Par[A]): A = ???
+    def run[A](pa: Par[A])(s: ExecutorService): Future[A] = pa(s)
 
-    def map2[A, B, C](pa: Par[A], pb: Par[B])(f: (A, B) => C): Par[C] = ???
+    def map2[A, B, C](pa: Par[A], pb: Par[B])(f: (A, B) => C): Par[C] =
+      es => UnitFuture(f(pa(es).get(), pb(es).get))
+
+    // def of map in term of map2
+    def map[A, B](pa: Par[A])(f: A => B): Par[B] =
+      map2(pa, unit(()))((a, _) => f(a))
+
+    def parMap[A,B](la: List[A])(f: A => B): Par[List[B]] =
+      Par.sequence(la.map(asyncF(f)))
+
+    def sequence[A](lp: List[Par[A]]): Par[List[A]] =
+      lp.foldRight(unit(List.empty[A]))((p, pl) => map2(p, pl)(_ :: _))
+
+    // take a list of int and sort it
+    def sortedMap(li: Par[List[Int]]): Par[List[Int]] = map(li)(_.sorted)
+
+    def asyncF[A, B](f: A => B): A => Par[B] = (a: A) => lazyUnit(f(a))
+
+    private case class UnitFuture[A](get: A) extends Future[A] {
+      override def cancel(mayInterruptIfRunning: Boolean): Boolean = false
+
+      override def get(timeout: Long, unit: TimeUnit): A = get
+
+      override def isCancelled: Boolean = false
+
+      override def isDone: Boolean = true
+    }
 
   }
 
@@ -39,7 +73,7 @@ object Part_2_7 {
       Par.unit(s.headOption getOrElse 0)
     else {
       val (l, r) = s.splitAt(s.length / 2)
-     Par.map2(Par.fork(sumP(l)), Par.fork(sumP(r)))(_ + _)
+      Par.map2(Par.fork(sumP(l)), Par.fork(sumP(r)))(_ + _)
     }
   }
 
